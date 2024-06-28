3d50d1aaa7a9d524b6a1ec3a52d7a56c97e9c3c9
IS0moto <k.iso20000814@gmail.com>
2024-06-29 03:56:56 +0900

    fix bug

--- navigation_tools/src/navigation_tools/nav_tool_lib.py
+++ navigation_tools/src/navigation_tools/nav_tool_lib.py
@@ -33,6 +33,7 @@ class NavModule:
         self.pub_move_rel = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
         self.pub_dist_angle = rospy.Publisher('/simple_move/goal_dist_angle', Float32MultiArray, queue_size=1)
         self.pub_robot_stop = rospy.Publisher('/navigation/stop', Empty, queue_size=1)
+        self.pub_robot_stop = rospy.Publisher('/navigation/stop', Empty, queue_size=1)
 
         #for lib env manage
         #self.pub_robot_stop = rospy.Publisher('/lme/first_rotate', Bool, queue_size=1)
