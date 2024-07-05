cc7d3591d66cbb75816ecd608774734fb416fdeb
ryohei.kobayashi <kobayashi.ryohei621@mail.kyutech.jp>
2024-07-05 19:59:00 +0900

    delete temp files

--- navigation/augment_gridmap_online/build/catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/roboworks/catkin_ws/src/pumas-navigation-docker/navigation/augment_gridmap_online/include".split(';') if "/home/roboworks/catkin_ws/src/pumas-navigation-docker/navigation/augment_gridmap_online/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
-PROJECT_NAME = "augment_gridmap_online"
-PROJECT_SPACE_DIR = "/home/roboworks/catkin_ws/src/pumas-navigation-docker/navigation/augment_gridmap_online/build/devel"
-PROJECT_VERSION = "0.0.0"
--- navigation/augment_gridmap_online/build/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest
-# Build directory: /home/roboworks/catkin_ws/src/pumas-navigation-docker/navigation/augment_gridmap_online/build/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
